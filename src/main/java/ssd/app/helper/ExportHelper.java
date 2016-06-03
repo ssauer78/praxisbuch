9cf075bf85cb30a820cbee2d6fff99aa928756b4
sauer <sauer@embl.de>
2016-06-03 08:20:26 +0200

    added the expenses to the exported excel sheet

--- src/main/java/ssd/app/helper/ExportHelper.java
+++ src/main/java/ssd/app/helper/ExportHelper.java
@@ -22,6 +22,7 @@ import org.slf4j.LoggerFactory;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import ssd.app.model.Appointment;
+import ssd.app.model.Expense;
 
 public class ExportHelper {
 	private static final Logger LOGGER = LoggerFactory.getLogger(ExportHelper.class);
@@ -37,9 +38,11 @@ public class ExportHelper {
 	 */
 	public static void exportForYear(int year, String to){
 		List<Appointment> appointments = new ArrayList<Appointment>();
+		List<Expense> expenses = new ArrayList<Expense>();
 		try {
 			appointments = AppointmentHelper.getInstance().getAppointments(year); // get all appointments
-			writeToExcel(appointments, to);
+			expenses = ExpensesHelper.getInstance().getExpenses(year);
+			writeToExcel(appointments, expenses, to);
 		} catch (SQLException e1) {
 			// ignore => just show nothing
 			LOGGER.error(e1.getMessage());
@@ -58,14 +61,14 @@ public class ExportHelper {
 	 * @param appointments
 	 * @param path
 	 */
-	public static void writeToExcel(List<Appointment> appointments, String path){
+	public static void writeToExcel(List<Appointment> appointments, List<Expense> expenses, String path){
         // Using XSSF for xlsx format, for xls use HSSF
         Workbook workbook = new XSSFWorkbook();
 
-        Sheet apointmentSheet = workbook.createSheet("Export");
+        Sheet incomeSheet = workbook.createSheet("Export");
         
         // write header line
-        Row row = apointmentSheet.createRow(0);
+        Row row = incomeSheet.createRow(0);
         row.createCell(0).setCellValue("Datum");
         row.createCell(1).setCellValue("Beschreibung");
         row.createCell(2).setCellValue("Dauer");
@@ -74,9 +77,10 @@ public class ExportHelper {
         row.createCell(5).setCellValue("Patient");
         row.createCell(6).setCellValue("Geburtsdatum");
 
+        double income = 0;
         int rowIndex = 1;
         for(Appointment appointment : appointments){
-            row = apointmentSheet.createRow(rowIndex++);
+            row = incomeSheet.createRow(rowIndex++);
             int cellIndex = 0;
             //first place in row is date
             Calendar cal = Calendar.getInstance();
@@ -88,7 +92,9 @@ public class ExportHelper {
             // third place in row is the duration
             row.createCell(cellIndex++).setCellValue(appointment.getDuration());
             // third place in row is the price
-            row.createCell(cellIndex++).setCellValue(appointment.getDuration() * appointment.getService().getCostPerUnit());
+            double _income = appointment.getDuration() * appointment.getService().getCostPerUnit();
+            income += _income;
+            row.createCell(cellIndex++).setCellValue(_income);
             // fourth place in row is the service
             row.createCell(cellIndex++).setCellValue(appointment.getService().getName());
             // fifth place in row is the patient
@@ -98,7 +104,41 @@ public class ExportHelper {
             String bdate = LocalDate.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DAY_OF_MONTH)).toString();
             row.createCell(cellIndex++).setCellValue(bdate);
         }
-
+        
+        row = incomeSheet.createRow(rowIndex++);
+        row.createCell(0).setCellValue("Gesamte Einnahmen: ");
+        row.createCell(1).setCellValue(income);
+        
+        row = incomeSheet.createRow(rowIndex++);	// blank line
+        
+        double overallcost = 0;
+        for(Expense expense : expenses){
+        	row = incomeSheet.createRow(rowIndex++);
+            int cellIndex = 0;
+            // first place in row is date
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(expense.getDate().getTime());
+            String date = LocalDate.of(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DAY_OF_MONTH)).toString();
+            row.createCell(cellIndex++).setCellValue(date);
+            // second row is the cost
+            overallcost += expense.getCost();
+            row.createCell(cellIndex++).setCellValue(expense.getCost());
+            // third row is the name
+            row.createCell(cellIndex++).setCellValue(expense.getName());
+            // forth row is the description
+            row.createCell(cellIndex++).setCellValue(expense.getDescription());
+        }
+        
+        row = incomeSheet.createRow(rowIndex++);	// blank line
+        row = incomeSheet.createRow(rowIndex++);
+        row.createCell(0).setCellValue("Gesamte Ausgaben: ");
+        row.createCell(1).setCellValue(overallcost);
+        
+        row = incomeSheet.createRow(rowIndex++);
+        row.createCell(0).setCellValue("Einnamen - Ausgaben: ");
+        row.createCell(1).setCellValue(income - overallcost);
+        
+        
         //write this workbook in excel file.
     	Path _path = Paths.get(path, "export.xlsx");
         try {
